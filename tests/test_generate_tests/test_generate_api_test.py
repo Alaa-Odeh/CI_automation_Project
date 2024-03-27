42931a43290bc001a148c5317f191bdef11d8e45
Alaa-Odeh <Kharbosh.computer@gmail.com>
2024-03-27 13:06:55 +0200

    update requierments

--- tests/test_generate_tests/test_generate_api_test.py
+++ tests/test_generate_tests/test_generate_api_test.py
@@ -9,18 +9,26 @@ def goals_api():
     yield api
     api.delete_goal()
 # Use pytest.mark.parametrize to create variations
-@pytest.mark.parametrize(
-    "goal_name, skills, levels, hours_per_week", [
-        ("Game developer", ["HTML", "C#", "C++"], ["Professional", "Advanced", "Intermediate"], 10),
-        ("Frontend developer", [ "Python","HTML"], ["Beginner", "Intermediate"], 10)
-                # Add more variations here as tuples
-    ])
-def test_generate_api_test(goals_api, goal_name, skills, levels, hours_per_week):
-    goals_api.post_new_goal(goal_name, skills, levels, hours_per_week)
-    skill_names, skill_levels, updated_hours_per_week = goals_api.get_goal_info()
 
-    assert sorted(skill_names) == sorted(skills), "Skills do not match"
-    assert sorted(skill_levels) == sorted(levels), "Levels do not match"
-    assert updated_hours_per_week == hours_per_week, "Weekly hours not updated"
+class TestGoalsAPIGenerate():
+    @pytest.mark.parametrize(
+        "goal_name, skills, levels, hours_per_week", [
+            ("Game developer", ["HTML", "C#", "C++"], ["Professional", "Advanced", "Intermediate"], 10),
+            ("Frontend developer", ["Python", "HTML"], ["Beginner", "Intermediate"], 10)
+            # Add more variations here as tuples
+        ])
+    def test_generate_api_test(self,goals_api, goal_name, skills, levels, hours_per_week):
+        goals_api.post_new_goal(goal_name, skills, levels, hours_per_week)
+        skill_names, skill_levels, updated_hours_per_week = goals_api.get_goal_info()
 
-    goals_api.delete_goal()
+        assert sorted(skill_names) == sorted(skills), "Skills do not match"
+        assert sorted(skill_levels) == sorted(levels), "Levels do not match"
+        assert updated_hours_per_week == hours_per_week, "Weekly hours not updated"
+
+        goals_api.delete_goal()
+
+    def setup_method(self):
+        self.goals_api = GoalsAPI()
+        self.goals_api.delete_goal()
+    def teardown_method(self):
+        self.goals_api.delete_goal()
